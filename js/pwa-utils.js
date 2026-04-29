b1890deb9c93427511559380b7540b7cffc1d050
perezramiroa <perez.ramiro.a@gmail.com>
2026-04-29 14:52:43 -0300

    Refactor PWAUtils class for better readability

--- js/pwa-utils.js
+++ js/pwa-utils.js
@@ -8,7 +8,7 @@ class PWAUtils {
   static registerServiceWorker() {
     if ('serviceWorker' in navigator) {
       window.addEventListener('load', () => {
-        navigator.serviceWorker.register('/sw.js')
+        navigator.serviceWorker.register('sw.js')
           .then(registration => {
             console.log('[PWA] Service Worker registrado:', registration.scope);
           })
